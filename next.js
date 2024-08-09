ba550cb9c8582d9a8d4591484f5c8f71f42a3a4b
Caio Sendin <sendindev@gmail.com>
2024-08-09 19:24:54 -0300

    feat: add Next.js config

---
+++ next.js
@@ -0,0 +1,58 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: [
+        'standard',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'jsx-a11y',
+        '@typescript-eslint',
+        'simple-import-sort',
+    ],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'prettier/prettier': ['error', {
+            'printWidth': 120,
+            'tabWidth': 4,
+            'singleQuote': true,
+            'trailingComma': 'all',
+            'arrowParens': 'always',
+            'semi': false,
+            'endOfLine': 'auto',
+        }],
+        'jsx-a11y/alt-text': [
+            'warn',
+            {
+                elements: ['img'],
+                img: ['Image'],
+            },
+        ],
+        'jsx-a11y/aria-props': 'warn',
+        'jsx-a11y/aria-proptypes': 'warn',
+        'jsx-a11y/aria-unsupported-elements': 'warn',
+        'jsx-a11y/role-has-required-aria-props': 'warn',
+        'jsx-a11y/role-supports-aria-props': 'warn',
+        'react/no-unknown-property': 'error',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        'import/parsers': {
+            [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+        },
+    }
+}
